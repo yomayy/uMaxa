b062c44a53ad983875a5468d05944aab4a1d8be3
Max Pustovoit <39699639+yomayy@users.noreply.github.com>
2020-08-29 00:14:48 +0300

    #0 Retreive product

---
+++ Shop.Application/Products/GetProduct.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.Application.Products
+{
+	public class GetProduct
+	{
+		private ApplicationDbContext _context;
+
+		public GetProduct(ApplicationDbContext context) {
+			_context = context;
+		}
+
+		public ProductViewModel Do(string name) {
+			return _context.Products
+				.Include(x => x.Stocks)
+				.Where(x => x.Name == name)
+				.Select(p => new ProductViewModel {
+					Name = p.Name,
+					Description = p.Description,
+					Value = $"{p.Value.ToString("N2")} $", // 1100.50 => 1,100.50 => $ 1,100.50
+					Stocks = p.Stocks.Select(y => new StockViewModel {
+						Id = y.Id,
+						Description = y.Description,
+						InStock = y.Quantity > 0
+					})
+				})
+				.FirstOrDefault();
+		}
+
+		public class ProductViewModel
+		{
+			public string Name { get; set; }
+			public string Description { get; set; }
+			public string Value { get; set; }
+			public IEnumerable<StockViewModel> Stocks { get; set; }
+		}
+
+		public class StockViewModel
+		{
+			public Guid Id { get; set; }
+			public string Description { get; set; }
+			public bool InStock { get; set; }
+		}
+	}
+}
---
+++ Shop.UI/Pages/Product.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Database;
+using Shop.Application.Products;
+
+namespace Shop.UI.Pages
+{
+    public class ProductModel : PageModel
+    {
+		private ApplicationDbContext _context;
+
+		public ProductModel(ApplicationDbContext context) {
+            _context = context;
+		}
+
+        public GetProduct.ProductViewModel Product { get; set; }
+
+        public IActionResult OnGet(string name)
+        {
+            Product = new GetProduct(_context).Do(name.Replace("-", " "));
+            if(Product == null) {
+                return RedirectToPage("Index");
+			}
+			else {
+                return Page();
+			}
+        }
+    }
+}
